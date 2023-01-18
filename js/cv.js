0ea9dabe568ee0d91ed1227b70a9012f4bdcb6d4
Ivan Ochoa <ivanochoa229@gmail.com>
2023-01-18 19:44:36 -0300

    Argentina programa 4

---
+++ js/cv.js
@@ -0,0 +1,17 @@
+document.getElementById('link').onclick = function()
+{
+    console.log("Redirigiendo hacia el Facebook de Jeanette Fox");
+    document.getElementById("face").innerHTML = "Redirigiendo hacia el Facebook de Jeanette Fox";
+}
+
+document.getElementById('link').onclick = function()
+{
+    console.log("Redirigiendo hacia el Facebook de Jeanette Fox");
+    document.getElementById("insta").innerHTML = "Redirigiendo hacia el Facebook de Jeanette Fox";
+}
+
+document.getElementById('link').onclick = function()
+{
+    console.log("Redirigiendo hacia el Facebook de Jeanette Fox");
+    document.getElementById("twt").innerHTML = "Redirigiendo hacia el Facebook de Jeanette Fox";
+}
