55c39dd65cb6295ad5af8966adf4d2b4da8d902f
Ivan Ochoa <ivanochoa229@gmail.com>
2023-02-20 15:42:38 -0300

    Correccion

--- js/cv.js
+++ js/cv.js
@@ -1,17 +1,17 @@
-document.getElementById('link').onclick = function()
+document.getElementById('link1').onclick = function()
 {
     console.log("Redirigiendo hacia el Facebook de Jeanette Fox");
     document.getElementById("face").innerHTML = "Redirigiendo hacia el Facebook de Jeanette Fox";
 }
 
-document.getElementById('link').onclick = function()
+document.getElementById('link2').onclick = function()
 {
-    console.log("Redirigiendo hacia el Facebook de Jeanette Fox");
-    document.getElementById("insta").innerHTML = "Redirigiendo hacia el Facebook de Jeanette Fox";
+    console.log("Redirigiendo hacia el Instagram de Jeanette Fox");
+    document.getElementById("insta").innerHTML = "Redirigiendo hacia el Instagram de Jeanette Fox";
 }
 
-document.getElementById('link').onclick = function()
+document.getElementById('link3').onclick = function()
 {
-    console.log("Redirigiendo hacia el Facebook de Jeanette Fox");
-    document.getElementById("twt").innerHTML = "Redirigiendo hacia el Facebook de Jeanette Fox";
+    console.log("Redirigiendo hacia el Twitter de Jeanette Fox");
+    document.getElementById("twt").innerHTML = "Redirigiendo hacia el Twitter de Jeanette Fox";
 }
